76c66fabb50efa5c6aa3eebdc48cf809098f44e9
Jeremy Poulter <jeremy@bigjungle.net>
2015-09-10 21:11:21 +0100

    Added some comments to explain what is going on

--- emonowl.js
+++ emonowl.js
@@ -1,21 +1,41 @@
+// -----------------------------
+// Load all the required modules
+
+// Module to parse data from the OWL Intuition 
 var OWL = require('owlintuition');
+
+// Some helpful utilities for debugging
 var util = require('util');
+
+// Module for making HTTP connections
 var http = require('http');
+
+// URL parsing module
 var url = require('url');
+
+// Configuration file management
 var config = require('config');
-var querystring = require('querystring');
 
 // Get the config into some handy vars
 var feeds = config.get('feeds');
 var nodes = config.get('nodes');
-
 var debug = config.has('debug') && config.get('debug');
-var log = debug ? console.log : function () {};
 
+// If debug is enabled then map the log function to console.log else map it to a dummy function
+var log = debug ? console.log : function () { };
+
+// Create an instance of the OWL Intuition listener
 var owl = new OWL();
 log("Starting monitor");
 owl.monitor();
 
+// Handle the electricity event. 
+//
+// Sends the raw data from the electricity monitor. This is essentially the same data that an emonTX
+// would send. 
+// 
+// We will pull out the three channels of data and a few other key bits of data (signal strength, 
+// battery level) and send them to emonCMS.
 owl.on('electricity', function( event ) {
   data = JSON.parse(event);
   log( "electricity = " + util.inspect(data, {"depth": null}) );
@@ -33,6 +53,19 @@ owl.on('electricity', function( event ) {
   reportToEmon(nodes.electricity, packet);
 });
 
+// Handle the solar event
+//
+// A simplified version of the electricity event with just the PV generation and 
+// consumption data. Since all this is in the electicity event we do not need to 
+// do anything with this event.
+owl.on('solar', function (event) {
+    log("solar = " + util.inspect(JSON.parse(event), { "depth": null }));
+});
+
+// Handle the heating event.
+//
+// Status data from the heating controller. Again pull out some key data and send
+// to emonCMS.
 owl.on('heating', function( event ) {
   data = JSON.parse(event);
   log( "heating = " + util.inspect(data, {"depth": null}) );
@@ -48,14 +81,18 @@ owl.on('heating', function( event ) {
   reportToEmon(nodes.heating, packet);
 });
 
+// Eandle the weather event.
+//
+// This is info on the current weather state, just data pulled from the internet.
+// We can retrieve in more detail directly from source as needed, so we will ignore 
+// this.
 owl.on('weather', function( event ) {
   log( "weather = " + util.inspect(JSON.parse(event), {"depth": null}) );
 });
 
-owl.on('solar', function( event ) {
-  log( "solar = " + util.inspect(JSON.parse(event), {"depth": null}) );
-});
-
+// Send data to emonCMS.
+//
+// Sends the packet with the passed in node ID to all the configured emonCMS servers.
 function reportToEmon(node, packet)
 {
   log( "node = " + node );
@@ -65,11 +102,12 @@ function reportToEmon(node, packet)
   }
 }
 
+// Make a HTTP connection to a particular server and send the data.
 function postData(urlString, node, key, packet)
 {
   urlParsed = url.parse(urlString);
 
-  // EmonCMS does not look to like properly encoded query strings...
+  // EmonCMS does not look to like properly decoded query strings...
   var post_data = 'json='+JSON.stringify(packet)+
                   '&apikey='+key+
                   '&node='+node;
